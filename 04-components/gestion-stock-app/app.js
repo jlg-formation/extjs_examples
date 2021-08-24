66c8556974023301b2fe48299f911b52bea7fd87
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-24 12:14:11 +0200

    dev: example with a simple container

--- 04-components/gestion-stock-app/app.js
+++ 04-components/gestion-stock-app/app.js
@@ -1,4 +1,25 @@
-Ext.application({
-	extend: 'GestionStockApp.Application',
-	name: 'GestionStockApp'
+function removeSplash() {
+  Ext.getBody().removeCls("launching");
+  var elem = document.getElementById("splash");
+  elem.parentNode.removeChild(elem);
+}
+
+Ext.onReady(() => {
+  removeSplash();
+
+  Ext.create("Ext.container.Container", {
+    renderTo: Ext.getBody(),
+    items: [
+      {
+        xtype: "datefield",
+        name: "startDate",
+        fieldLabel: "Start date",
+      },
+      {
+        xtype: "datefield",
+        name: "endDate",
+        fieldLabel: "End date",
+      },
+    ],
+  });
 });
