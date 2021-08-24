13131343620c2568176041692c55408a8096ecf6
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-24 11:38:05 +0200

    dev: merge the class examples with component example

--- 03-structures/gestion-stock-app/app.js
+++ 03-structures/gestion-stock-app/app.js
@@ -1,3 +1,49 @@
+console.log("GestionStockApp: ", GestionStockApp);
+console.log("about to override Person");
+
+// 1) use the prototype (not recommanded)
+// GestionStockApp.utils.Person.prototype.sayHello = function () {
+//   console.log(`New version: Hello my name is ${this.name}`);
+// };
+
+// 2) use Ext.Base.override
+// GestionStockApp.utils.Person.override({
+//   sayHello: function () {
+//     this.callParent();
+//     console.log("I am glad to meet you.");
+//   },
+// });
+
+// 3) use Ext.override
+Ext.override(GestionStockApp.utils.Person, {
+  sayHello: function () {
+    this.callParent();
+    console.log("I am VERY glad to meet you.");
+  },
+});
+
+const alice = Ext.create("GestionStockApp.utils.Person");
+alice.sayHello();
+alice.sleep();
+const bob = Ext.create("GestionStockApp.utils.Person", "Bob");
+bob.sayHello();
+
+// Not recommanded because you cannot always be sure the class has been already well defined.
+// Please get the habbit of using Ext.create
+const charly = new GestionStockApp.utils.Employee("Charly");
+charly.sayHello();
+
+const dany = Ext.create("widget.employee", "Dany");
+dany.sayHello();
+dany.setSalary(1300);
+console.log("dany salary: " + dany.getSalary());
+try {
+  dany.setSalary("toto");
+} catch (e) {
+  console.log("e: ", e);
+}
+console.log("Employee count: " + GestionStockApp.utils.Employee.count);
+
 function removeSplash() {
   Ext.getBody().removeCls("launching");
   var elem = document.getElementById("splash");
