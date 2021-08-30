1bb8b8ff0042b3078383bc98aaa32cfb4cd14b9c
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-08-30 13:13:35 +0200

    dev: layout card

--- 06-routing/gestion-stock-app/app/desktop/src/view/layout/body/BodyView.js
+++ 06-routing/gestion-stock-app/app/desktop/src/view/layout/body/BodyView.js
@@ -2,9 +2,5 @@ Ext.define("GestionStockApp.view.layout.BodyView", {
   extend: "Ext.Container",
   xtype: "bodyview",
   requires: ["Ext.Label"],
-  layout: {
-    type: "vbox",
-    align: "center",
-    pack: "center",
-  },
+  layout: "card",
 });
